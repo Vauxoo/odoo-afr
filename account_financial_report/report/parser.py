c441f0930df744738a13c5b0f1805040745a493e
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-08-08 12:44:00 -0430

    [ADD] add the parameter group_by given in the wizard and integrating to the way that the report is print.

--- account_financial_report/report/parser.py
+++ account_financial_report/report/parser.py
@@ -355,9 +355,13 @@ class account_balance(report_sxw.rml_parse):
         ctx = ctx or {}
         res = self._get_analytic_ledger(account, ctx=ctx)
         if res:
-            res2 = self.aml_group_by_keys(res, ['currency', 'partner'])
-            partner_total_list = self.get_group_total(res2.values(), total_str='{partner}', main_group='currency', remove_lines=True)
-            return self.get_group_total(partner_total_list, main_group='currency', total_str='TOTAL IN {currency}')
+            if ctx['group_by'] == 'currency':
+                res2 = self.aml_group_by_keys(res, ['currency', 'partner'])
+                partner_total_list = self.get_group_total(res2.values(), total_str='{partner}', main_group='currency', remove_lines=True)
+                return self.get_group_total(partner_total_list, main_group='currency', total_str='TOTAL IN {currency}')
+            else:
+                res2 = self.aml_group_by_keys(res, ['partner', 'currency'])
+                return self.get_group_total(res2.values(), total_str='{partner} Total in {currency}', main_group='partner', remove_lines=True)
         else:
             return []
 
