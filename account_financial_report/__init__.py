d7d0b8c0e6b4ebe50aea71c3a109ddfa3f3c7d2d
Humberto Arocha <hbto@vauxoo.com>
2014-12-20 07:28:06 -0430

    [FLAKE8]

--- account_financial_report/__init__.py
+++ account_financial_report/__init__.py
@@ -3,14 +3,14 @@
 #    Module Writen to OpenERP, Open Source Management Solution
 #    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
 #    All Rights Reserved
-###############Credits######################################################
+# Credits #################################################################
 #    Coded by:   Humberto Arocha humberto@openerp.com.ve
 #                Angelica Barrios angelicaisabelb@gmail.com
 #               Jordi Esteve <jesteve@zikzakmedia.com>
 #    Planified by: Humberto Arocha
 #    Finance by: LUBCAN COL S.A.S http://www.lubcancol.com
 #    Audited by: Humberto Arocha humberto@openerp.com.ve
-#############################################################################
+###########################################################################
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -23,7 +23,7 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##############################################################################
+###########################################################################
 
 from . import model
 from . import report
