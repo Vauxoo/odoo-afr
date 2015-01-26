bc73796c2fe0af8df386f1b5779f4dadab3cf326
Humberto Arocha <hbto@vauxoo.com>
2015-01-26 16:43:34 -0430

    [IMP] Five Columns report in qweb has landed

--- account_financial_report/report/parser.py
+++ account_financial_report/report/parser.py
@@ -1761,12 +1761,16 @@ report_sxw.report_sxw(
     parser=account_balance,
     header=False)
 
-report_sxw.report_sxw(
-    'report.afr.5cols',
-    'wizard.report',
-    'account_financial_report/report/balance_full_5_cols.rml',
-    parser=account_balance,
-    header=False)
+class report_afr_5_cols(osv.AbstractModel):
+
+    # _name = `report.` + `report_name`
+    # report_name="afr.5cols"
+    _name = 'report.afr.5cols'
+
+    # this inheritance will allow to render this particular report
+    _inherit = 'report.abstract_report'
+    _template = 'account_financial_report.afr_template'
+    _wrapped_report_class = account_balance
 
 report_sxw.report_sxw(
     'report.afr.qtrcols',
