691408b2a29900dc2483b555096171a7a5b3b4b7
Hugo Adan <hugo@vauxoo.com>
2015-04-10 12:09:17 -0500

    [ADD][ account_afr_group_auditory]add menu_acces and permisions to Auditor (Read-only) group

---
+++ account_afr_group_auditory/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Vauxoo (<http://vauxoo.com>).
+#    All Rights Reserved
+# ##############Credits######################################################
+#    Coded by: Hugo Francisco Adan Oliva(hugo@vauxoo.com)
+#############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+{
+    "name": "Grand read permissios to Auditor (Read-Only) group"
+            "over account_financial_report.afr",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Accounting",
+    "description": """
+    This module add menus_acces to Auditor (Read-Only) group
+    and grand permissios over afr model
+    """,
+    "website": "http://www.vauxoo.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "account",
+        "account_financial_report",
+        "account_user_audit",
+    ],
+    "demo": [],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
